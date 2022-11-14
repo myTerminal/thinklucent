cb01457cd8d198a04eba013ff750654fdad7b845
myTerminal <team.terminal@gmail.com>
2022-11-14 12:43:48 +0530

    create the missing top-level 'main' function

--- src/main.lisp
+++ src/main.lisp
@@ -7,16 +7,17 @@
 
 (defparameter *counter* 10)
 
-(bt:make-thread
- (lambda ()
-   (loop
-    (sleep 1)
-    (if (<= *counter* 0)
-        (enable-or-disable-lights nil)
-        (decf *counter*)))))
+(defun main ()
+  "The main entry point to the program."
+  (bt:make-thread
+   (lambda ()
+     (loop
+      (sleep 1)
+      (if (<= *counter* 0)
+          (enable-or-disable-lights nil)
+          (decf *counter*)))))
 
-(with-inotify (inotify T ("/dev/input" :all-events))
-  (do-events (event inotify :blocking-p T)
-    (setf *counter* 10)
-    (enable-or-disable-lights t)))
-
+  (with-inotify (inotify T ("/dev/input" :all-events))
+    (do-events (event inotify :blocking-p T)
+      (setf *counter* 10)
+      (enable-or-disable-lights t))))
